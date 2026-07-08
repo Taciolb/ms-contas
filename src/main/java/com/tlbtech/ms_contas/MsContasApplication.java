44891d04edccb66530c9d26ab4839020d35f106d
Taciano <taciolb@gmail.com>
2026-07-07 21:53:42 -0300

    feat: commit inicial

---
+++ src/main/java/com/tlbtech/ms_contas/MsContasApplication.java
@@ -0,0 +1,13 @@
+package com.tlbtech.ms_contas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsContasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tlbtech/ms_contas/MsContasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlbtech.ms_contas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
